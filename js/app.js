defb4a84824d94f115bb440b98621c5ee75ebbff
Tamannarahman <tamannarahmanmridha@gmail.com>
2022-08-23 11:32:32 +0600

    player section added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,28 @@
 const selectArray = [];
+
+function display(playerName) {
+    const listIteam = document.getElementById('player-name');
+    listIteam.innerHTML = '';
+
+    for (let i = 0; i < playerName.length; i++) {
+
+        const name = selectArray[i].playerName;
+        const ol = document.createElement('li');
+        ol.innerHTML = `<li> ${i + 1} ${name}</li>
+        
+        `;
+
+        listIteam.appendChild(ol);
+    }
+}
+
 function addToSelect(element) {
     const playerName = element.parentNode.parentNode.children[1].innerText;
     const playerObj = {
         playerName: playerName
     }
     selectArray.push(playerObj)
-    console.log(selectArray)
-    console.log(selectArray.length)
+    display(selectArray);
+
 }
 
