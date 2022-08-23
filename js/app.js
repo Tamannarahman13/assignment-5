6b2a37caf66b9db934047e882a3a9ecddc92edde
Tamannarahman <tamannarahmanmridha@gmail.com>
2022-08-23 18:52:45 +0600

    player expenses clculate added

--- js/app.js
+++ js/app.js
@@ -22,18 +22,64 @@ function addToSelect(element) {
         playerName: playerName
     }
     selectArray.push(playerObj)
+    document.getElementById('selected-player').innerText = selectArray.length
     display(selectArray);
+
     element.disabled = true;
 
 }
 
-
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    const selectPlayer = document.getElementById('player-name');
-    const selectPlayerNumber = selectPlayer.innerText;
+    const perPlayerFieldElement = document.getElementById('per-player-field');
+    const perPlayerString = perPlayerFieldElement.value;
+    const perPlayer = parseInt(perPlayerString);
+    perPlayerFieldElement.value = perPlayer;
 
-    const selectPlayerString = parseInt(selectPlayerNumber)
+    const selectedPlayerElement = document.getElementById('selected-player');
+    const selectedPlayerString = selectedPlayerElement.innerText;
+    const selectedPlayer = parseInt(selectedPlayerString);
+    selectedPlayerElement.innerText = selectedPlayer;
 
-    console.log(selectPlayerString)
+    const playerExpensesElement = document.getElementById('player-expenses');
+    const playerExpensesString = playerExpensesElement.innerText;
+    const playerExpenses = parseInt(playerExpensesString);
+
+    const totalExpenses = perPlayer * selectedPlayer;
+    playerExpensesElement.innerText = totalExpenses;
+
+})
+
+
+
+
+
+
+
+document.getElementById('calculate-total').addEventListener('click', function () {
+    const playerExpensesElement = document.getElementById('player-expenses');
+    const playerExpensesString = playerExpensesElement.innerText;
+    const playerExpenses = parseInt(playerExpensesString);
+    playerExpensesElement.innerText = playerExpenses;
+
+
+    const managerAmountElement = document.getElementById('manager-amount');
+    const managerAmountString = managerAmountElement.value;
+    const managerAmount = parseInt(managerAmountString);
+    managerAmountElement.value = managerAmount;
+
+
+    const coachAmountElement = document.getElementById('coach-amount');
+    const coachAmountString = coachAmountElement.value;
+    const coachAmount = parseInt(coachAmountString);
+    coachAmountElement.value = coachAmount;
+
+
+    const totalElement = document.getElementById('total');
+    const totalString = totalElement.innerText;
+    const total = parseInt(totalString);
+    const totalAmount = playerExpenses + managerAmount + coachAmount;
+    totalElement.innerText = totalAmount;
+
+
 })
 
